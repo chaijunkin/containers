8a563571b37c8e96fe4a5c05b483c6472fc30f34
Roxedus <me@roxedus.dev>
2024-03-14 21:23:43 +0000

    fix(debian): dont look for sha

--- apps/debian/ci/latest.sh
+++ apps/debian/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 channel=bookworm-slim
-version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name + "@" + .digest' 2>/dev/null | head -n1)
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name' 2>/dev/null | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 version="${version%_*}"
