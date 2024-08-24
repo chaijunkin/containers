7a5eb2f1c417aa095c5b2ac1b8abce73d571f2eb
chaijunkin <chaijunkin@gmail.com>
2024-08-24 23:43:45 +0800

    fix: go back to old container approach

--- apps/jbops/entrypoint.sh
+++ apps/jbops/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-#shellcheck disable=SC1091
-test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
-
 exec \
-    /usr/bin/python3 \
+    /usr/local/bin/python \
         "/app/${JBOPS__SCRIPT_PATH}" \
         "$@"
