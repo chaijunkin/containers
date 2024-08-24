faff5926db2d76287b37422ec820431fdf1c6207
chaijunkin <chaijunkin@gmail.com>
2024-08-24 23:25:00 +0800

    fix: try copy pasta

--- apps/bazarr/entrypoint.sh
+++ apps/bazarr/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-#shellcheck disable=SC1091
-test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
-
 #shellcheck disable=SC2086
 exec \
-    /usr/bin/python3 \
+    /usr/local/bin/python \
         /app/bazarr.py \
             --no-update \
             --config /config \
