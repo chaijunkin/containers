5d185738dbae5e2d8233efdf608b876d0e393ac1
Roxedus <me@roxedus.dev>
2025-05-30 15:04:07 +0000

    fix: pin rallly to foss lisence

--- apps/rallly/ci/latest.sh
+++ apps/rallly/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/lukevella/rallly/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' | sed 's|^v||')"
+#version="$(curl -sX GET "https://api.github.com/repos/lukevella/rallly/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' | sed 's|^v||')"
+version="3.11.2"
 printf "%s" "${version}"
